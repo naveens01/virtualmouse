8b3b5ff4541b061d9dc717e52b807437efd4e2cf
NAVEEN S <88920707+naveens01@users.noreply.github.com>
2022-10-24 09:54:13 +0530

    Update action.py

--- action.py
+++ action.py
@@ -4,8 +4,7 @@ import numpy as np
 import HandTrackingModule as htm
 import time
 import autopy
- 
-##########################
+
 wCam, hCam = 640, 480
 frameR = 100 # Frame Reduction
 smoothening = 7
